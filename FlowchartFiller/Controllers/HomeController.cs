99ce26b0c161ae67db41f503ae710dfd06768208
John Karaplis <johnkaraplis@Johns-MacBook-Pro.local>
2018-07-15 22:01:12 -0400

    testing text entry

--- FlowchartFiller/Controllers/HomeController.cs
+++ FlowchartFiller/Controllers/HomeController.cs
@@ -15,27 +15,7 @@ namespace FlowchartFiller.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+            public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
